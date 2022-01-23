8b3292e62778c35531ae3072cd61d3a188bae63a
Kruglov Andrei <dus.sneg@gmail.com>
2022-01-23 15:35:38 +0500

    Initial commit

---
+++ src/Main_15_4_5.java
@@ -0,0 +1,24 @@
+import java.util.Optional;
+import java.util.Random;
+
+public class Main_15_4_5 {
+
+    private static Optional<String> checkGuess(int guess) {
+        if (guess < 1 || guess > 5) {
+            throw new IllegalStateException();// проверяем на граничные условия
+        } else {
+            int result = new Random().nextInt(5) + 1;// генерируем рандом
+            System.out.println(result);
+            if (result == guess) {// делаем проверку
+                return Optional.of("Поздравляем! Вы угадали значение");
+            } else {
+                return Optional.empty();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(checkGuess(3));
+    }
+}
